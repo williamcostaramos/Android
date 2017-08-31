e167d76aff9e3966d9b2e8a71c1a88775cdbe1e4
william Ramos <wulliamcostaramos@gmail.com>
2017-08-31 18:48:54 -0300

    first commit

---
+++ app/src/main/java/com/example/catolica2017/sistemalogin/Login.java
@@ -0,0 +1,27 @@
+package com.example.catolica2017.sistemalogin;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+public class Login extends AppCompatActivity {
+
+    private EditText txtLogin;
+    private EditText txtPwd;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+
+
+
+    public void trataEvento(View views){
+
+
+    }
+
+}
---
+++ app/src/main/java/com/example/catolica2017/sistemalogin/Profile.java
@@ -0,0 +1,13 @@
+package com.example.catolica2017.sistemalogin;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by williamramos on 31/08/17.
+ */
+
+public class Profile extends AppCompatActivity {
+
+
+
+}
